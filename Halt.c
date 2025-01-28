41b409ad1e8084abadd7aee7ca134b2bcb91520b
Harish Charan <charan.harish@gmail.com>
2025-01-28 22:50:30 +0000

    Made HaltCondition global parameter and input from the input-data

--- Halt.c
+++ Halt.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "globalExtern.h" //Definitions of global variables
-void HaltConditionCheck(double value, int stepCount){
-double HaltCondition;
-HaltCondition = 1e-6;
- if(value <= HaltCondition && value != 0){
- fprintf(fpresult, "Halt condition met at step = %d with Vrms = %.10f\n", stepCount, value);
- fclose(fpresult);  // Close the results file
- exit(0);           // Terminate the program
- } }
+#include "globalExtern.h" // Definitions of global variables
+bool HaltConditionCheck(double value, int stepCount) {
+  
+  if(value <= HaltCondition && value != 0) {
+  fprintf(fpresult, "Halt condition met at step = %d with Vrms = %.10f\n", stepCount, value);
+  return true;       // Signal that the halt condition is met
+  }
+ return false; // Halt condition not met
+}
+
