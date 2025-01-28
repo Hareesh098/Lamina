38660778869893ebe4725336992e1cda64d668fd
Harish Charan <charan.harish@gmail.com>
2025-01-28 22:52:52 +0000

    Removed the evaluation part and made it to print only

--- PrintVrms.c
+++ PrintVrms.c
@@ -3,24 +3,8 @@
 #include<math.h>
 #include"globalExtern.h"
 void PrintVrms(){
-  int n;
-  VSqr = 0.0;
-  VMeanSqr = 0.0;
-  VRootMeanSqr = 0.0;
-  
-  if(stepCount == stepTraj){
-  fprintf(fpvrms, "#timeNow\tVrms \n");
-  }
-  
-  for(n = 1 ; n <= nAtom ; n ++){
-  VSqr += Sqr(vx[n]) + Sqr(vy[n]); 
-  }
-  VMeanSqr = VSqr/nAtom;
-  VRootMeanSqr = sqrt(VMeanSqr);
-  
   fprintf(fpvrms, "%0.16lf\t%0.16lf\n", timeNow, VRootMeanSqr);
   fflush(fpvrms);
-  
   }
   
   
