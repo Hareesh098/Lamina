e6920cda080cd717f10ea1d0b376e034bf955e55
Harish Charan <charan.harish@gmail.com>
2025-01-28 22:47:21 +0000

    Replaced the ro[BondType[n]] to ro[BondID[n]] and remved the bondLength and bondEnergy

--- DumpBonds.c
+++ DumpBonds.c
@@ -11,10 +11,10 @@ void DumpBonds(){
   fprintf(fpbond, "%lf %lf xlo xhi\n", -regionH[1], regionH[1]);
   fprintf(fpbond, "%lf %lf ylo yhi\n", -regionH[2], regionH[2]);
   fprintf(fpbond, "%lf %lf zlo zhi\n", -0.1, 0.1);
-  fprintf(fpbond, "ITEM: ENTRIES BondID, BondType, atom1 atom2 kb ro BondEnergy BondLength\n");
+  fprintf(fpbond, "ITEM: ENTRIES BondID, BondType, atom1 atom2 kb ro\n");
 
   for(n=1; n<=nBond; n++)
-   fprintf(fpbond, "%d %d %d %d %lf %lf %lf %lf\n", BondID[n], BondType[n], atom1[n], atom2[n], kb[n], ro[n], BondEnergy[n], BondLength[n]);
+   fprintf(fpbond, "%d %d %d %d %lf %lf\n", BondID[n], BondType[n], atom1[n], atom2[n], kb[BondID[n]], ro[BondID[n]]);
   }
 
 
