4516010d541ff6579585f2c2284f1984f575b073
Harish Charan <charan.harish@gmail.com>
2025-01-28 23:00:14 +0000

    Prints Center of Mass of particles as per EvalCom()

---
+++ PrintCom.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include"globalExtern.h"
+void PrintCom(){
+
+  fprintf(fpcom, "%0.16lf\t%0.16lf\t%0.16lf\n", timeNow, ComX, ComY);
+  fflush(fpcom);
+  
+  }
+  
+  
+  
