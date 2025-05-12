51badbda80fa925d1b6cd2dc33c24d7beccc5460
Harish Charan <charan.harish@gmail.com>
2025-05-12 16:23:15 +0100

    Added dampFlag = 3, improved array index, added a feature to remove pair interaction between bonded atoms

--- ComputeBondForce.c
+++ ComputeBondForce.c
@@ -5,26 +5,33 @@
 
 void ComputeBondForce(){
   int n;
-  double strech;
   double dr[NDIM+1], r, rr, ri, roi;
-  double uVal,fcVal;
+  double uVal, fcVal;
 
-  uVal = 0.0; fcVal = 0.0; TotalBondEnergy = 0.0;
-  virSumBond = 0.0; virSumBondxx = 0.; virSumBondyy = 0.; virSumBondxy = 0.;
-  gamman = 1.0;
+  uVal = 0.0; TotalBondEnergy = 0.0;
+  virSumBond = 0.0; virSumBondxx = 0.0; virSumBondyy = 0.0; virSumBondxy = 0.0;
 
-  double vr[NDIM+1], fdVal, rrinv;
-  rrinv = 0.0;  fdVal = 0.0; 
+  double vr[NDIM+1], fdVal, rri;
+ 
+  for(n = 1 ; n <= nAtom ; n ++){
+   nodeDragx[n] = 0.0;
+   nodeDragy[n] = 0.0;
+  } //Important change made on 03Apr2025. Mention it in GitHub
 
+  int atom1ID, atom2ID;
+   
   for(n=1; n<=nBond; n++){
-
-   dr[1] = rx[atom1[n]] - rx[atom2[n]];
+   rr = 0.0; rri = 0.0; fcVal = 0.0;  fdVal = 0.0; strech = 0.0;
+   atom1ID = atom1[n];
+   atom2ID = atom2[n];
+   
+   dr[1] = rx[atom1ID] - rx[atom2ID];
    if(dr[1] >= regionH[1]) 
     dr[1] -= region[1];
    else if(dr[1] < -regionH[1]) 
     dr[1] += region[1];
 
-   dr[2] = ry[atom1[n]] - ry[atom2[n]];
+   dr[2] = ry[atom1ID] - ry[atom2ID];
    if(dr[2] >= regionH[2]){
     dr[1] -= shearDisplacement;
     if(dr[1] < -regionH[1]) dr[1] += region[1];
@@ -37,26 +44,74 @@ void ComputeBondForce(){
 	
    rr = Sqr(dr[1]) + Sqr(dr[2]);
    r = sqrt(rr);
-   rrinv = 1.0/rr;
+   rri = 1.0/rr;
    ri = 1.0/r;
-   roi = 1.0/ro[BondID[n]];
-   strech = 1.0 - r * roi;
-   uVal = 0.5 * kb[BondID[n]] * ro[BondID[n]] * Sqr(strech);
-   vr[1] = vx[atom1[n]] - vx[atom2[n]];
-   vr[2] = vy[atom1[n]] - vy[atom2[n]];
-   fcVal = -kb[BondID[n]] * strech * ri; //F = -Grad U
-   fdVal = -gamman * (vr[1]*dr[1] + vr[2]*dr[2]) * rrinv; //node-node drag
-
-   nodeDragx[atom1[n]] +=  fdVal * dr[1];  //node-node drag
-   nodeDragy[atom1[n]] +=  fdVal * dr[2];  //node-node drag
-   nodeDragx[atom2[n]] +=  -fdVal * dr[1];  //node-node drag
-   nodeDragy[atom2[n]] +=  -fdVal * dr[2];  //node-node drag
-      
-   ax[atom1[n]] +=  (fcVal + fdVal) * dr[1];
-   ay[atom1[n]] +=  (fcVal + fdVal) * dr[2];
-   ax[atom2[n]] += -(fcVal + fdVal) * dr[1];
-   ay[atom2[n]] += -(fcVal + fdVal) * dr[2];
+   roi = 1.0/ro[n];
+   strech = (r * roi - 1.0);
+   uVal = 0.5 * kb[n] * ro[n] * Sqr(strech);
+   fcVal = -kb[n] * strech * ri; //F = -Grad U
    
+   vr[1] = vx[atom1ID] - vx[atom2ID];
+   vr[2] = vy[atom1ID] - vy[atom2ID];
+   fdVal = -gamman * (vr[1]*dr[1] + vr[2]*dr[2]) * rri; //node-node drag
+   
+   //DampFlag = 1. LAMMPS version
+   if(DampFlag == 1){
+   nodeDragx[atom1ID] =  fdVal * dr[1];  //node-node drag  //Important change made on 03Apr2025. Mention it in GitHub
+   nodeDragy[atom1ID] =  fdVal * dr[2];  //node-node drag  //Adding the drag forces is wrong. Only add the 
+   nodeDragx[atom2ID] =  -fdVal * dr[1];  //node-node drag //total force 
+   nodeDragy[atom2ID] =  -fdVal * dr[2];  //node-node drag
+      
+   ax[atom1ID] +=  (fcVal + fdVal) * dr[1];
+   ay[atom1ID] +=  (fcVal + fdVal) * dr[2];
+   ax[atom2ID] += -(fcVal + fdVal) * dr[1];
+   ay[atom2ID] += -(fcVal + fdVal) * dr[2];
+   }
+   
+   //DampFlag = 2. Suzanne notes version
+   else if(DampFlag == 2){
+   nodeDragx[atom1ID] =  -gamman * vr[1];  //node-node drag
+   nodeDragy[atom1ID] =  -gamman * vr[2]; //node-node drag
+   nodeDragx[atom2ID] =  -(-gamman * vr[1]);  //node-node drag
+   nodeDragy[atom2ID] =  -(-gamman * vr[2]);  //node-node drag
+      
+   ax[atom1ID] +=  (fcVal * dr[1] - gamman * vr[1]);
+   ay[atom1ID] +=  (fcVal * dr[2] - gamman * vr[2]);
+   ax[atom2ID] += -(fcVal * dr[1] - gamman * vr[1]);
+   ay[atom2ID] += -(fcVal * dr[2] - gamman * vr[2]);
+   }
+
+   //DampFlag = 3. Suzanne PRL, 130, 178203 (2023) version
+   else if(DampFlag == 3){
+    DeltaXijNew = dr[1];
+    DeltaYijNew = dr[2];
+
+    if(stepCount == 0) {  // First timestep
+     DeltaXijOld[n] = DeltaXijNew;
+     DeltaYijOld[n] = DeltaYijNew;
+    }
+
+    DeltaXij = DeltaXijNew - DeltaXijOld[n];
+    DeltaYij = DeltaYijNew - DeltaYijOld[n];
+    DeltaVXij = DeltaXij / deltaT;
+    DeltaVYij = DeltaYij / deltaT;
+   
+    // Now update for the next timestep
+    DeltaXijOld[n] = DeltaXijNew;
+    DeltaYijOld[n] = DeltaYijNew;
+
+    nodeDragx[atom1ID] =  -gamman * DeltaVXij;  //node-node drag
+    nodeDragy[atom1ID] =  -gamman * DeltaVYij; //node-node drag
+    nodeDragx[atom2ID] =  -(-gamman * DeltaVXij);  //node-node drag
+    nodeDragy[atom2ID] =  -(-gamman * DeltaVYij);  //node-node drag
+      
+    ax[atom1ID] +=  (fcVal * dr[1] - gamman * DeltaVXij);
+    ay[atom1ID] +=  (fcVal * dr[2] - gamman * DeltaVYij);
+    ax[atom2ID] += -(fcVal * dr[1] - gamman * DeltaVXij);
+    ay[atom2ID] += -(fcVal * dr[2] - gamman * DeltaVYij);
+   }
+
+
    BondLength[n] = r;
    BondEnergy[n] = uVal; //No 0.5 factor since it is the energy of the bond
    TotalBondEnergy   +=  BondEnergy[n]; 
