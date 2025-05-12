622752709e4181e4736d46d1d448ef7eb3d889ce
Harish Charan <charan.harish@gmail.com>
2025-05-12 16:48:00 +0100

    printing the time upto 4 decimal points

--- PrintVrms.c
+++ PrintVrms.c
@@ -3,7 +3,7 @@
 #include<math.h>
 #include"globalExtern.h"
 void PrintVrms(){
-  fprintf(fpvrms, "%0.16lf\t%0.16lf\n", timeNow, VRootMeanSqr);
+  fprintf(fpvrms, "%0.4lf\t%0.16lf\n", timeNow, VRootMeanSqr);
   fflush(fpvrms);
   }
   
