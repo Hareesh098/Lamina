c1c17d9256ae85d52f6839aa33a4c9e7b6a90f80
Harish Charan <charan.harish@gmail.com>
2025-05-12 16:38:58 +0100

    Increased the numerical precision

--- PrintSummary.c
+++ PrintSummary.c
@@ -2,7 +2,7 @@
 #include"globalExtern.h"
 
 void PrintSummary(){
-  fprintf(fpresult, "%0.13lf %0.13lf %0.13lf %0.13lf %0.13lf %0.13lf %0.13lf %0.13lf %0.13lf\n", 
-   timeNow, vSum, potEnergy, kinEnergy, totEnergy, uSumPairPerAtom, BondEnergyPerAtom, sPressure, svirSum);
+  fprintf(fpresult, "%0.4lf\t%0.16lf\t%0.16lf\t%0.16lf\t%0.16lf\t%0.16lf\t%0.16lf\t%0.16lf\t%0.16lf\n", 
+   timeNow, vSum, potEnergy, kinEnergy, totEnergy, uSumPairPerAtom, BondEnergyPerAtom, pressure, virSum);
   fflush(fpresult);
 }
