9cb8e493e07575b1a85465aa2dc4752747739387
Harish Charan <charan.harish@gmail.com>
2025-05-12 16:37:24 +0100

    Increased the numerical precision

--- PrintCom.c
+++ PrintCom.c
@@ -3,8 +3,7 @@
 #include<math.h>
 #include"globalExtern.h"
 void PrintCom(){
-
-  fprintf(fpcom, "%0.16lf\t%0.16lf\t%0.16lf\n", timeNow, ComX, ComY);
+  fprintf(fpcom, "%0.4lf\t%0.16lf\t%0.16lf\n", timeNow, ComX, ComY);
   fflush(fpcom);
   
   }
