c817c555b80a24c4b999454d92dacaa48147d986
Harish Charan <charan.harish@gmail.com>
2025-05-12 16:38:33 +0100

    Increased the numerical precision

--- PrintStress.c
+++ PrintStress.c
@@ -2,6 +2,6 @@
 #include"globalExtern.h"
 
 void PrintStress(){
-  fprintf(fpstress, "%0.7lf %0.16lf %0.16lf %0.16lf %0.16lf\n", timeNow, virSumxx, virSumyy, virSumxy, pressure);
+  fprintf(fpstress, "%0.4lf\t%0.16lf\t%0.16lf\t%0.16lf\t%0.16lf\n", timeNow, virSumxx, virSumyy, virSumxy, pressure);
   fflush(fpstress);
 }
