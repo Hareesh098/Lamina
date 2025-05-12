e92e851e428b04cf8a6631b4a9cf7fc5b316f420
Harish Charan <charan.harish@gmail.com>
2025-05-12 16:55:37 +0100

    Made the Unwrap flag more accurate. Now it works well for very very fast atoms, that cross the bounderies more than one in 1 timestep

--- EvalUnwrap.c
+++ EvalUnwrap.c
@@ -6,8 +6,6 @@
 void EvalUnwrap() {
  int n;
  for (n = 1; n <= nAtom; n++) {
-  ImageX[n] += (rx[n] >= regionH[1]) - (rx[n] < -regionH[1]);
-  ImageY[n] += (ry[n] >= regionH[2]) - (ry[n] < -regionH[2]);
   rxUnwrap[n] = rx[n] + ImageX[n] * region[1];
   ryUnwrap[n] = ry[n] + ImageY[n] * region[2];
   }
--- VelocityVerletStep.c
+++ VelocityVerletStep.c
@@ -10,7 +10,22 @@ int n;
   vy[n] += ay[n] * 0.5 * deltaT;
   rx[n] += vx[n] * deltaT; 
   ry[n] += vy[n] * deltaT;
-  } } }
+  } 
+  //Calculating the image flags here
+  if (rx[n] >= regionH[1]) {
+   rx[n] -= region[1];
+   ImageX[n]++;
+   } else if (rx[n] < -regionH[1]) {
+   rx[n] += region[1];
+  ImageX[n]--;
+  }
+  if (ry[n] >= regionH[2]) {
+   ry[n] -= region[2];
+   ImageY[n]++;
+   } else if (ry[n] < -regionH[2]) {
+   ry[n] += region[2];
+   ImageY[n]--;
+   } } }
   else if(icode == 2){
   for(n = 1; n <= nAtom; n++) {
   if(atomType[n] != freezeAtomType){
