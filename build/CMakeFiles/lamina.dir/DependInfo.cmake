1038dd0ab50faf2d6d63d691ff7bbfcf12b5cc4e
Harish Charan <charan.harish@gmail.com>
2025-05-23 14:30:11 +0100

    added CMake and ./build

---
+++ build/CMakeFiles/lamina.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hcharan/GitHub/Lamina/source/AccumProps.c" "CMakeFiles/lamina.dir/source/AccumProps.c.o" "gcc" "CMakeFiles/lamina.dir/source/AccumProps.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/AccumVacf.c" "CMakeFiles/lamina.dir/source/AccumVacf.c.o" "gcc" "CMakeFiles/lamina.dir/source/AccumVacf.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/AllocArrays.c" "CMakeFiles/lamina.dir/source/AllocArrays.c.o" "gcc" "CMakeFiles/lamina.dir/source/AllocArrays.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/ApplyBoundaryCond.c" "CMakeFiles/lamina.dir/source/ApplyBoundaryCond.c.o" "gcc" "CMakeFiles/lamina.dir/source/ApplyBoundaryCond.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/ApplyDrivingForce.c" "CMakeFiles/lamina.dir/source/ApplyDrivingForce.c.o" "gcc" "CMakeFiles/lamina.dir/source/ApplyDrivingForce.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/ApplyForce.c" "CMakeFiles/lamina.dir/source/ApplyForce.c.o" "gcc" "CMakeFiles/lamina.dir/source/ApplyForce.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/ApplyLeesEdwardsBoundaryCond.c" "CMakeFiles/lamina.dir/source/ApplyLeesEdwardsBoundaryCond.c.o" "gcc" "CMakeFiles/lamina.dir/source/ApplyLeesEdwardsBoundaryCond.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/ApplyShear.c" "CMakeFiles/lamina.dir/source/ApplyShear.c.o" "gcc" "CMakeFiles/lamina.dir/source/ApplyShear.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/ApplyViscous.c" "CMakeFiles/lamina.dir/source/ApplyViscous.c.o" "gcc" "CMakeFiles/lamina.dir/source/ApplyViscous.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/BrownianStep.c" "CMakeFiles/lamina.dir/source/BrownianStep.c.o" "gcc" "CMakeFiles/lamina.dir/source/BrownianStep.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/Close.c" "CMakeFiles/lamina.dir/source/Close.c.o" "gcc" "CMakeFiles/lamina.dir/source/Close.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/ComputeBondForce.c" "CMakeFiles/lamina.dir/source/ComputeBondForce.c.o" "gcc" "CMakeFiles/lamina.dir/source/ComputeBondForce.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/ComputeForcesCells.c" "CMakeFiles/lamina.dir/source/ComputeForcesCells.c.o" "gcc" "CMakeFiles/lamina.dir/source/ComputeForcesCells.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/ComputePairForce.c" "CMakeFiles/lamina.dir/source/ComputePairForce.c.o" "gcc" "CMakeFiles/lamina.dir/source/ComputePairForce.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/DisplaceAtoms.c" "CMakeFiles/lamina.dir/source/DisplaceAtoms.c.o" "gcc" "CMakeFiles/lamina.dir/source/DisplaceAtoms.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/DumpBonds.c" "CMakeFiles/lamina.dir/source/DumpBonds.c.o" "gcc" "CMakeFiles/lamina.dir/source/DumpBonds.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/DumpPairs.c" "CMakeFiles/lamina.dir/source/DumpPairs.c.o" "gcc" "CMakeFiles/lamina.dir/source/DumpPairs.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/DumpRestart.c" "CMakeFiles/lamina.dir/source/DumpRestart.c.o" "gcc" "CMakeFiles/lamina.dir/source/DumpRestart.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/DumpState.c" "CMakeFiles/lamina.dir/source/DumpState.c.o" "gcc" "CMakeFiles/lamina.dir/source/DumpState.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/EvalCom.c" "CMakeFiles/lamina.dir/source/EvalCom.c.o" "gcc" "CMakeFiles/lamina.dir/source/EvalCom.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/EvalProps.c" "CMakeFiles/lamina.dir/source/EvalProps.c.o" "gcc" "CMakeFiles/lamina.dir/source/EvalProps.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/EvalRdf.c" "CMakeFiles/lamina.dir/source/EvalRdf.c.o" "gcc" "CMakeFiles/lamina.dir/source/EvalRdf.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/EvalSpacetimeCorr.c" "CMakeFiles/lamina.dir/source/EvalSpacetimeCorr.c.o" "gcc" "CMakeFiles/lamina.dir/source/EvalSpacetimeCorr.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/EvalUnwrap.c" "CMakeFiles/lamina.dir/source/EvalUnwrap.c.o" "gcc" "CMakeFiles/lamina.dir/source/EvalUnwrap.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/EvalVacf.c" "CMakeFiles/lamina.dir/source/EvalVacf.c.o" "gcc" "CMakeFiles/lamina.dir/source/EvalVacf.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/EvalVrms.c" "CMakeFiles/lamina.dir/source/EvalVrms.c.o" "gcc" "CMakeFiles/lamina.dir/source/EvalVrms.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/Halt.c" "CMakeFiles/lamina.dir/source/Halt.c.o" "gcc" "CMakeFiles/lamina.dir/source/Halt.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/Init.c" "CMakeFiles/lamina.dir/source/Init.c.o" "gcc" "CMakeFiles/lamina.dir/source/Init.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/InitVacf.c" "CMakeFiles/lamina.dir/source/InitVacf.c.o" "gcc" "CMakeFiles/lamina.dir/source/InitVacf.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/Integrate.c" "CMakeFiles/lamina.dir/source/Integrate.c.o" "gcc" "CMakeFiles/lamina.dir/source/Integrate.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/LeapfrogStep.c" "CMakeFiles/lamina.dir/source/LeapfrogStep.c.o" "gcc" "CMakeFiles/lamina.dir/source/LeapfrogStep.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/PrintCom.c" "CMakeFiles/lamina.dir/source/PrintCom.c.o" "gcc" "CMakeFiles/lamina.dir/source/PrintCom.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/PrintMomentum.c" "CMakeFiles/lamina.dir/source/PrintMomentum.c.o" "gcc" "CMakeFiles/lamina.dir/source/PrintMomentum.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/PrintStress.c" "CMakeFiles/lamina.dir/source/PrintStress.c.o" "gcc" "CMakeFiles/lamina.dir/source/PrintStress.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/PrintSummary.c" "CMakeFiles/lamina.dir/source/PrintSummary.c.o" "gcc" "CMakeFiles/lamina.dir/source/PrintSummary.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/PrintVacf.c" "CMakeFiles/lamina.dir/source/PrintVacf.c.o" "gcc" "CMakeFiles/lamina.dir/source/PrintVacf.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/PrintVrms.c" "CMakeFiles/lamina.dir/source/PrintVrms.c.o" "gcc" "CMakeFiles/lamina.dir/source/PrintVrms.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/SetupJob.c" "CMakeFiles/lamina.dir/source/SetupJob.c.o" "gcc" "CMakeFiles/lamina.dir/source/SetupJob.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/Trajectory.c" "CMakeFiles/lamina.dir/source/Trajectory.c.o" "gcc" "CMakeFiles/lamina.dir/source/Trajectory.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/VelocityVerletStep.c" "CMakeFiles/lamina.dir/source/VelocityVerletStep.c.o" "gcc" "CMakeFiles/lamina.dir/source/VelocityVerletStep.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/ZeroVacf.c" "CMakeFiles/lamina.dir/source/ZeroVacf.c.o" "gcc" "CMakeFiles/lamina.dir/source/ZeroVacf.c.o.d"
+  "/home/hcharan/GitHub/Lamina/source/main.c" "CMakeFiles/lamina.dir/source/main.c.o" "gcc" "CMakeFiles/lamina.dir/source/main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/lamina.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for lamina.
+# This may be replaced when dependencies are built.
